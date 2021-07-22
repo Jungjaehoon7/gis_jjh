c4e17e2703f47f919878870e9e35611a3c70a051
JungJaehoon <topeshock77@likelion.org>
2021-07-22 10:17:57 +0900

    20210722_2 COMMIT

--- accountapp/views.py
+++ accountapp/views.py
@@ -9,8 +9,10 @@ from django.urls import reverse, reverse_lazy
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
+from accountapp.decorators import account_ownership_required
 from accountapp.forms import AccountCreationForm
 from accountapp.models import HelloWorld
+
 
 @login_required(login_url=reverse_lazy('accountapp:login'))
 def hello_world(request):
@@ -45,9 +47,10 @@ class AccountDetailView(DetailView):
     template_name = 'accountapp/detail.html'
 
 
+has_ownership = [login_required, account_ownership_required]
 
-@method_decorator(login_required, 'get')
-@method_decorator(login_required, 'post')
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 
 class AccountUpdateView(UpdateView):
     model = User
@@ -56,8 +59,9 @@ class AccountUpdateView(UpdateView):
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
 
-@method_decorator(login_required, 'get')
-@method_decorator(login_required, 'post')
+
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 
 class AccountDeleteView(DeleteView):
     model = User
