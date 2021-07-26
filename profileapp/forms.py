935a4087703ad4511c05756b62bd84cb3b47e0db
JungJaehoon <topeshock77@likelion.org>
2021-07-26 10:47:03 +0900

    20210726_3 COMMIT

--- profileapp/forms.py
+++ profileapp/forms.py
@@ -3,8 +3,9 @@ from django.forms import ModelForm
 from profileapp.models import Profile
 
 
-class ProfileCrearionForm(ModelForm):
+class ProfileCreationForm(ModelForm):
     class Meta:
         model = Profile
         fields = ['image', 'nickname', 'message']
+
 
